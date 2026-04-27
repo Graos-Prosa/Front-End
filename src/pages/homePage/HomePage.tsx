79e6ed5df01e23f229c8f7d0b931504466dce782
Juan Pablo da Paixão Santos <juan.santos@al.infnet.edu.br>
2026-04-27 10:50:20 -0300

    feat: Primeira versão da barra de progresso adicionada

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,10 +1,23 @@
 import Navbar from "../../components/navbar/Navbar";
+import TrackingBar from "../../components/trackingBar/TrackingBar";
+
+import { FaTruck, FaShoppingCart, FaCreditCard, FaFlag } from "react-icons/fa";
 
 export default function HomePage() {
+  const steps = [
+    { label: "Items", icon: FaShoppingCart },
+    { label: "Delivery", icon: FaTruck },
+    { label: "Payment", icon: FaCreditCard },
+    { label: "Finish", icon: FaFlag },
+  ];
+
   return (
     <div className="divDebug">
-      <Navbar type={"primary"} itensInCart={3} />
+      <Navbar type={"secondary"} itensInCart={2} />
       <p className="pDebug">Página inicial</p>
+      <div style={{ width: "80%", marginTop: "30px"}}>
+        <TrackingBar steps={steps} currentStep={3} />
+      </div>
     </div>
   );
 }
