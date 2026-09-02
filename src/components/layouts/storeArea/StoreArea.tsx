198f1065bac15a125a7a487c7b32eead54f6b33a
Juan Pablo da Paixão Santos <juan.santos@al.infnet.edu.br>
2026-09-02 08:38:25 -0300

    Inicio da página de ProdutosPage.tsx.

--- src/components/layouts/storeArea/StoreArea.tsx
+++ src/components/layouts/storeArea/StoreArea.tsx
@@ -6,13 +6,15 @@ import { useNavigate } from "react-router-dom";
 
 //mock
 import { productsMock } from "../../../data/mocks/Products.mock";
+import Input from "../../common/input/Input";
 
 interface StoreAreaProps {
     style?: React.CSSProperties;
     maxProducts?: number;
+    productsPage?: boolean;
 }
 
-export default function StoreArea({ style, maxProducts = 0 }: StoreAreaProps) {
+export default function StoreArea({ style, maxProducts = 0, productsPage = false }: StoreAreaProps) {
     type FilterType = "Todos" | "Acessorios" | "Cafés";
 
     const [selectedFilter, setSelectedFilter] = useState<FilterType>("Todos");
@@ -54,14 +56,32 @@ export default function StoreArea({ style, maxProducts = 0 }: StoreAreaProps) {
     return (
         <section id={styles.storeArea} style={{...style}}>
             <div className={styles.storeContent}>
-                <h3>Conheça nossos grãos selecionados</h3>
-                <p>Grãos selecionados, aroma intenso e sabor marcante. Um café equilibrado para transformar seu dia em um momento especial.</p>
+                {
+                    productsPage ?
+                        <>
+                            <h3>Descubra nossos produtos</h3>
+                            <p>Grãos selecionados, aroma intenso e sabor marcante. Um café equilibrado para transformar seu dia em um momento especial.</p>
+                        </>
+                    :
+                        <>
+                            <h3>Conheça nossos grãos selecionados</h3>
+                            <p>Grãos especiais e acessórios selecionados para elevar sua experiência. Tudo o que você precisa para preparar um café marcante, com qualidade e personalidade.</p>
+                        </>
+                }
             </div>
-            <div className={styles.filterArea}>
-                <Button type={handleSelectedButton("Todos")} textContent={"Todos"} onClick={() => handleFilterClick("Todos")} style={{width: "33%", padding: "10px", fontSize: "12px"}}/>
-                <Button type={handleSelectedButton("Acessorios")} textContent={"Acessorios"} onClick={() => handleFilterClick("Acessorios")} style={{width: "33%", padding: "10px", fontSize: "12px"}}/>
-                <Button type={handleSelectedButton("Cafés")} textContent={"Cafés"} onClick={() => handleFilterClick("Cafés")} style={{width: "33%", padding: "10px", fontSize: "12px"}}/>
-            </div>
+            {
+                productsPage ?
+                    <div className={styles.filterAreaProductsPage}>
+                        <Input type={"text"} style={{width: "100%", padding: "5px"}} styleType={"primary"} placeholder={"Buscar produto"} onChange={(e) => console.log(e.target.value)} value="" /> {/*Consertar dps aqui !!!!!*/}
+                        <Button type={handleSelectedButton("Todos")} textContent={"Test"} onClick={() => handleFilterClick("Todos")} style={{width: "33%", padding: "10px", fontSize: "12px"}}/> {/*Consertar dps aqui !!!!!*/}
+                    </div>
+                :
+                    <div className={styles.filterArea}>
+                        <Button type={handleSelectedButton("Todos")} textContent={"Todos"} onClick={() => handleFilterClick("Todos")} style={{width: "33%", padding: "10px", fontSize: "12px"}}/>
+                        <Button type={handleSelectedButton("Acessorios")} textContent={"Acessorios"} onClick={() => handleFilterClick("Acessorios")} style={{width: "33%", padding: "10px", fontSize: "12px"}}/>
+                        <Button type={handleSelectedButton("Cafés")} textContent={"Cafés"} onClick={() => handleFilterClick("Cafés")} style={{width: "33%", padding: "10px", fontSize: "12px"}}/>
+                    </div>
+                }
             <div className={styles.productsArea}>
                 {filteredProducts.slice(0, maxProducts).map((product) => (
                     <ProductCard
@@ -73,7 +93,7 @@ export default function StoreArea({ style, maxProducts = 0 }: StoreAreaProps) {
             <div className={styles.amountProductsArea}>
                 <p className={styles.amountProducts}><strong>{maxProducts} de {filteredProducts.length}</strong> produtos</p>
                 <ProgressBar current={maxProducts} total={filteredProducts.length}/>
-                <Button type={"secondary"} textContent="Ver todos os produtos" style={{width: "100%", padding: "10px", marginTop: "20px"}} onClick={goToProductsPage} />
+                <Button type={"secondary"} textContent="Ver todos os produtos" style={{width: "100%", padding: "10px", marginTop: "20px", display: productsPage ? "none" : "block"}} onClick={goToProductsPage} />
             </div>
         </section>
     );
--- src/pages/productsPage/ProductsPage.tsx
+++ src/pages/productsPage/ProductsPage.tsx
@@ -1,7 +1,28 @@
+import StoreArea from "../../components/layouts/storeArea/StoreArea";
+import Navbar from "../../components/common/navbar/Navbar";
+import FormLogin from "../../components/common/formLogin/FormLogin";
+import Footer from "../../components/common/footer/Footer";
+
+import styles from "./ProductsPage.module.css";
+
+
+//mock
+import { IS_LOGGED } from "../../data/mocks/User.mock"
+
 export default function ProductsPage() {
-    return (
-        <div>
-            <p>Teste products page</p>
-        </div>
-    )
+
+  return (
+    <div className={styles.homePage}>
+      
+      <Navbar type={"third"} itensInCart={2} />
+
+      <StoreArea maxProducts={6} productsPage={true} style={{marginBottom: "50px"}} />
+
+      {IS_LOGGED && <FormLogin />}
+
+      <Footer />
+
+    </div>
+    
+  );
 }
